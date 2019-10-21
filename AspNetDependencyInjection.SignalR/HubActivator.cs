116f8c5f2a5b85ab2f04996745a7a6040d89cfd4
Dai <dai@dai.me>
2019-10-21 01:09:35 -0700

    Adding our HubActivator and HubDispatcher implementations.

---
+++ AspNetDependencyInjection.SignalR/HubActivator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetDependencyInjection.Internal
+{
+	public class DependencyInjectionSignalRHubActivator : IHubActivator
+	{
+		private readonly ApplicationDependencyInjection di;
+		private readonly IServiceProvider rootServiceProvider;
+
+		public DependencyInjectionSignalRHubActivator( ApplicationDependencyInjection di, IServiceProvider rootServiceProvider )
+		{
+			this.di                  = di                  ?? throw new ArgumentNullException( nameof( di ) );
+			this.rootServiceProvider = rootServiceProvider ?? throw new ArgumentNullException( nameof( rootServiceProvider ) );
+		}
+
+		public IHub Create( HubDescriptor descriptor )
+		{
+			IServiceScope scope = DependencyInjectionSignalRHubDispatcher.GetScope();
+
+			// This fails because MEDI will *not* create a non-registered type, even if the type has a constructor where all parameters are registered.
+			// return (IHub)scope.ServiceProvider.GetRequiredService( serviceType: descriptor.HubType );
+
+			Object instantiated = this.di.ObjectFactoryCache.GetRequiredService( scope.ServiceProvider, descriptor.HubType );
+			return (IHub)instantiated;
+		}
+	}
+}
---
+++ AspNetDependencyInjection.SignalR/HubDispatcher.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetDependencyInjection.Internal
+{
+	/// <summary>The HubDispatcher manages DI scope over a Hub's lifetime.</summary>
+	public class DependencyInjectionSignalRHubDispatcher : HubDispatcher
+    {
+		private static readonly AsyncLocal<IServiceScope> _asyncLocalScope = new AsyncLocal<IServiceScope>();
+
+		private readonly ApplicationDependencyInjection di;
+		private readonly IServiceProvider               rootServiceProvider;
+
+        public DependencyInjectionSignalRHubDispatcher( ApplicationDependencyInjection di, IServiceProvider rootServiceProvider, HubConfiguration configuration ) 
+            : base(configuration)
+        {
+            this.di                  = di                  ?? throw new ArgumentNullException( nameof(di) );
+			this.rootServiceProvider = rootServiceProvider ?? throw new ArgumentNullException( nameof(rootServiceProvider) );
+        }
+
+        protected override async Task OnConnected( IRequest request, String connectionId )
+        {
+			using( IServiceScope scope = this.rootServiceProvider.CreateScope() )
+			{
+				SetScope( scope );
+
+				try
+				{
+					await base.OnConnected( request, connectionId );
+				}
+				finally
+				{
+					ReleaseScope( scope );
+				}
+			}
+        }
+
+        protected override async Task OnReceived(IRequest request, String connectionId, String data)
+        {
+			using( IServiceScope scope = this.rootServiceProvider.CreateScope() )
+			{
+				SetScope( scope );
+
+				try
+				{
+					await base.OnReceived( request, connectionId, data );
+				}
+				finally
+				{
+					ReleaseScope( scope );
+				}
+			}
+        }
+
+        protected override async Task OnDisconnected(IRequest request, String connectionId, Boolean stopCalled)
+        {
+			using( IServiceScope scope = this.rootServiceProvider.CreateScope() )
+			{
+				SetScope( scope );
+
+				try
+				{
+					await base.OnDisconnected( request, connectionId, stopCalled );
+				}
+				finally
+				{
+					ReleaseScope( scope );
+				}
+			}
+        }
+
+        protected override async Task OnReconnected(IRequest request, String connectionId)
+        {
+			using( IServiceScope scope = this.rootServiceProvider.CreateScope() )
+			{
+				SetScope( scope );
+
+				try
+				{
+					await base.OnReconnected( request, connectionId );
+				}
+				finally
+				{
+					ReleaseScope( scope );
+				}
+			}
+        }
+
+		private sealed class AsyncLocalScope : IDisposable
+		{
+			private readonly IServiceScope scope;
+
+			public static AsyncLocalScope Create( IServiceProvider rootServiceProvider )
+			{
+				if( rootServiceProvider == null ) throw new ArgumentNullException(nameof(rootServiceProvider));
+
+				IServiceScope scope = rootServiceProvider.CreateScope();
+				return new AsyncLocalScope( scope );
+			}
+
+			private AsyncLocalScope( IServiceScope scope )
+			{
+				this.scope = scope ?? throw new ArgumentNullException( nameof( scope ) );
+
+				SetScope( scope );
+			}
+
+			void IDisposable.Dispose()
+			{
+				ReleaseScope( this.scope );
+			}
+		}
+
+		private static void SetScope( IServiceScope scope )
+		{
+			if( scope == null ) throw new ArgumentNullException(nameof(scope));
+
+			if( _asyncLocalScope.Value != null )
+			{
+				throw new InvalidOperationException( "Scope has already been set for this Async context." );
+			}
+			else
+			{
+				_asyncLocalScope.Value = scope;
+			}
+		}
+
+		private static void ReleaseScope( IServiceScope scope )
+		{
+			if( scope == null ) throw new ArgumentNullException(nameof(scope));
+
+			if( Object.ReferenceEquals( _asyncLocalScope.Value, scope ) )
+			{
+				_asyncLocalScope.Value = null;
+			}
+			else if( _asyncLocalScope.Value == null )
+			{
+				throw new InvalidOperationException( "Current Async context has a null Scope object." );
+			}
+			else
+			{
+				throw new InvalidOperationException( "Current Async context has an unexpected Scope object set." );
+			}
+		}
+
+		public static IServiceScope GetScope()
+		{
+			if( _asyncLocalScope.Value == null )
+			{
+				throw new InvalidOperationException( "Current Async context has a null Scope object." );
+			}
+			else
+			{
+				return _asyncLocalScope.Value;
+			}
+		}
+    }
+}
