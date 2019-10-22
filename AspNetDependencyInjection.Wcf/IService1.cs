b3bdd79df04f120450ed6cf0b00127b4a2103061
Dai <dai@dai.me>
2019-10-21 21:54:55 -0700

    Adding WCF project demonstrating Service *.svc DI.

---
+++ AspNetDependencyInjection.Wcf/IService1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AspNetDependencyInjection.Wcf
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+	[ServiceContract]
+	public interface IService1
+	{
+
+		[OperationContract]
+		string GetData( int value );
+
+		[OperationContract]
+		CompositeType GetDataUsingDataContract( CompositeType composite );
+
+		// TODO: Add your service operations here
+	}
+
+
+	// Use a data contract as illustrated in the sample below to add composite types to service operations.
+	[DataContract]
+	public class CompositeType
+	{
+		bool boolValue = true;
+		string stringValue = "Hello ";
+
+		[DataMember]
+		public bool BoolValue
+		{
+			get { return boolValue; }
+			set { boolValue = value; }
+		}
+
+		[DataMember]
+		public string StringValue
+		{
+			get { return stringValue; }
+			set { stringValue = value; }
+		}
+	}
+
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+	public class Service1 : IService1
+	{
+		public string GetData( int value )
+		{
+			return string.Format( "You entered: {0}", value );
+		}
+
+		public CompositeType GetDataUsingDataContract( CompositeType composite )
+		{
+			if( composite == null )
+			{
+				throw new ArgumentNullException( "composite" );
+			}
+			if( composite.BoolValue )
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+	}
+}
---
+++ AspNetDependencyInjection.Wcf/WcfServiceDI/AndiWebServiceHostFactory.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Reflection;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace AspNetDependencyInjection.Wcf
+{
+	// All services are singletons or non-disposable transients (via factories) btw - no support for scoped services here.
+
+	public class AndiWebServiceHostFactory : WebServiceHostFactory
+	{
+		private class AndiWebObjectActivatorAndiServiceProvider : IServiceProvider
+		{
+			public Object GetService( Type serviceType )
+			{
+				return HttpRuntime.WebObjectActivator.GetService( serviceType );
+			}
+		}
+
+		private static readonly AndiWebObjectActivatorAndiServiceProvider _sp = new AndiWebObjectActivatorAndiServiceProvider();
+
+		protected override ServiceHost CreateServiceHost( Type serviceType, Uri[] baseAddresses )
+		{
+			return new AndiWebServiceHost( _sp, serviceType, baseAddresses );
+		}
+	}
+
+	public class AndiWebServiceHost : WebServiceHost
+	{
+		public AndiWebServiceHost( IServiceProvider serviceProvider, Type serviceType, params Uri[] baseAddresses )
+			: base( serviceType, baseAddresses )
+		{
+			if( serviceProvider == null ) throw new ArgumentNullException(nameof(serviceProvider));
+
+			//
+
+			ConstructorInfo[] ctors = serviceType.GetConstructors();
+			if( ctors.Length != 1 ) throw new InvalidOperationException( "Expected 1 public constructor for " + serviceType.FullName + "." );
+
+			foreach( ContractDescription contractDescription in this.ImplementedContracts.Values )
+			{
+				// Is it one IInstanceProvider-per-Contract, or per-Injected service, or per-WCF service?
+				contractDescription.ContractBehaviors.Add( new AndiInstanceProvider( serviceProvider ) );
+			}
+		}
+	}
+
+	public sealed class AndiInstanceProvider : IInstanceProvider, IContractBehavior
+	{
+		private readonly IServiceProvider serviceProvider;
+
+		public AndiInstanceProvider( IServiceProvider serviceProvider )
+		{
+			this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+		}
+
+		#region IContractBehavior
+
+		void IContractBehavior.AddBindingParameters( ContractDescription contractDescription, ServiceEndpoint endpoint, BindingParameterCollection bindingParameters )
+		{
+		}
+
+		void IContractBehavior.ApplyClientBehavior( ContractDescription contractDescription, ServiceEndpoint endpoint, ClientRuntime clientRuntime )
+		{
+		}
+
+		void IContractBehavior.ApplyDispatchBehavior( ContractDescription contractDescription, ServiceEndpoint endpoint, DispatchRuntime dispatchRuntime )
+		{
+			dispatchRuntime.InstanceProvider = this;
+		}
+
+		void IContractBehavior.Validate( ContractDescription contractDescription, ServiceEndpoint endpoint )
+		{
+		}
+
+		#endregion
+
+		#region IInstanceProvider
+
+		Object IInstanceProvider.GetInstance( InstanceContext instanceContext )
+		{
+			Type serviceType = instanceContext.Host.Description.ServiceType; // TODO: Is this an injected-service type or a WCF service type?
+			return this.serviceProvider.GetService( serviceType );
+		}
+
+		Object IInstanceProvider.GetInstance( InstanceContext instanceContext, Message message )
+		{
+			return ((IInstanceProvider)this).GetInstance( instanceContext );
+		}
+
+		void IInstanceProvider.ReleaseInstance( InstanceContext instanceContext, Object instance )
+		{
+			if( instance is IDisposable disposable )
+			{
+				disposable.Dispose();
+			}
+		}
+
+		#endregion
+
+
+	}
+}
