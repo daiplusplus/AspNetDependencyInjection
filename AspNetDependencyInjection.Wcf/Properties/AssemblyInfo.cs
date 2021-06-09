7287852997864b3908a6e21452ae1a9945de2a2f
Dai Rees <dai@dai.me>
2021-06-09 05:09:46 -0700

    Adding `InternalsVisibleTo` to the library assemblies to facilitate testing. And Shouldly.

--- AspNetDependencyInjection.Wcf/Properties/AssemblyInfo.cs
+++ AspNetDependencyInjection.Wcf/Properties/AssemblyInfo.cs
@@ -34,3 +34,10 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion( "1.0.0.0" )]
 [assembly: AssemblyFileVersion( "1.0.0.0" )]
+
+[assembly: InternalsVisibleTo( assemblyName: @"AspNetDependencyInjection.Tests, PublicKey=" +
+	"0024000004800000940000000602000000240000525341310004000001000100b5644bd32f0714" +
+	"dce56bb49d687880774726f85935f8213aff7d200ef6b75d6103996e8e26410b54475eeaeae3dd" +
+	"3b1f48900025d8e66a8beb6c0580416ca82a0ef3ee4794dc2c06aa0d5e23ca5e4efaef2c98cc66" +
+	"3efb5f00ef1c20b962a432e61dbbd5f672841d39de5b5ee2d157341d5d28a43c9bb7493ec838c8" +
+	"e94586a5" )]
--- AspNetDependencyInjection/Properties/AssemblyInfo.cs
+++ AspNetDependencyInjection/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -39,3 +40,11 @@ using System.Runtime.InteropServices;
 // Version 1.x = S. Kyle Korndoerfer's Unity.WebForms.
 // Version 2.x = Jehoel's Unity.WebForms using WebObjectActivator.
 // Version 3.x = AspNetDependencyInjection using Microsoft.Extensions.DependencyInjection.
+// Version 4.x = AspNetDependencyInjection using Microsoft.Extensions.DependencyInjection.
+
+[assembly: InternalsVisibleTo( assemblyName: @"AspNetDependencyInjection.Tests, PublicKey=" +
+	"0024000004800000940000000602000000240000525341310004000001000100b5644bd32f0714" +
+	"dce56bb49d687880774726f85935f8213aff7d200ef6b75d6103996e8e26410b54475eeaeae3dd" +
+	"3b1f48900025d8e66a8beb6c0580416ca82a0ef3ee4794dc2c06aa0d5e23ca5e4efaef2c98cc66" +
+	"3efb5f00ef1c20b962a432e61dbbd5f672841d39de5b5ee2d157341d5d28a43c9bb7493ec838c8" +
+	"e94586a5" )]
