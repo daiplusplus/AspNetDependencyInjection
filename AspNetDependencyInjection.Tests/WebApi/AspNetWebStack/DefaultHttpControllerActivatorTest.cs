3b3db8f2d930abbb97c63e30238fe0a85db1c03e
Dai Rees <dai@dai.me>
2021-07-18 15:21:39 -0700

    Adding tests appropriate from AspNetWebStack's test suite...

---
+++ AspNetDependencyInjection.Tests/WebApi/AspNetWebStack/DefaultHttpControllerActivatorTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dependencies;
+using System.Web.Http.Dispatcher;
+
+using AspNetDependencyInjection.Internal;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Shouldly;
+
+namespace AspNetDependencyInjection.Tests.WebApi
+{
+	/// <summary>Ripped from AspNetWebStack's test suite.</summary>
+	[TestClass]
+	public class DefaultHttpControllerActivatorTest : BaseWebApiWebStackTests
+	{
+		private class BeginScopeNull_DependencyResolver : IDependencyResolver
+		{
+			private readonly IDependencyResolver inner;
+
+			public BeginScopeNull_DependencyResolver( IDependencyResolver inner )
+			{
+				this.inner = inner;
+			}
+
+			public IDependencyScope BeginScope()
+			{
+				// This is the custom logic set in the original `Create_ThrowsForNullDependencyScope`.
+				return null;
+			}
+
+			public Object GetService( Type serviceType ) => this.inner.GetService( serviceType );
+
+			public IEnumerable<Object> GetServices( Type serviceType ) => this.inner.GetServices( serviceType );
+
+			public void Dispose()
+			{
+				this.inner.Dispose();
+			}
+		}
+
+		/// <summary>
+		/// <c>System.Web.Http.Dispatcher.DefaultHttpControllerActivatorTest</c><br />
+		/// <c>AspNetWebStack\test\System.Web.Http.Test\Dispatcher\DefaultHttpControllerActivatorTest.cs</c>
+		/// </summary>
+		[TestMethod]
+        public void Create_ThrowsForNullDependencyScope()
+        {
+			this.WrapTest( services => services.AddTransient<IActionValueBinder,TestActionValueBinder>(), this.Create_ThrowsForNullDependencyScope_Impl );
+		}
+
+        private void Create_ThrowsForNullDependencyScope_Impl( IDependencyResolver resolverOrig )
+        {
+			IDependencyResolver resolver2 = new BeginScopeNull_DependencyResolver( inner: resolverOrig );
+
+            // Arrange
+            HttpConfiguration config = new HttpConfiguration();
+            config.DependencyResolver = resolver2;
+
+			HttpRequestMessage request = new HttpRequestMessage();
+            request.SetConfiguration(config);
+
+            HttpControllerDescriptor descriptorSimpleController = new HttpControllerDescriptor(config, "Simple", typeof(SimpleController));
+
+			DefaultHttpControllerActivator activator = new DefaultHttpControllerActivator();
+
+            // Act & Assert
+			try
+			{
+				IHttpController controller = activator.Create( request, descriptorSimpleController, typeof(SimpleController) );
+				
+				true.ShouldBeFalse( "Method did not throw an exception." );
+			}
+			catch( InvalidOperationException ioEx )
+			{
+				ioEx.Message.ShouldBe( "An error occurred when trying to create a controller of type 'SimpleController'. Make sure that the controller has a parameterless public constructor." );
+
+				ioEx.InnerException.Message.ShouldBe(
+					"A dependency resolver of type 'ObjectProxy(_\\d+)?' returned an invalid value of null from its " +
+					"BeginScope method. If the container does not have a concept of scope, consider returning a scope " +
+					"that resolves in the root of the container instead."
+				);
+
+			}
+		}
+	}
+}
---
+++ AspNetDependencyInjection.Tests/WebApi/AspNetWebStack/DefaultServicesTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dependencies;
+using System.Web.Http.Dispatcher;
+using System.Web.Http.Filters;
+using System.Web.Http.Services;
+
+using AspNetDependencyInjection.Internal;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Shouldly;
+
+namespace AspNetDependencyInjection.Tests.WebApi
+{
+	/// <summary>Ripped from AspNetWebStack's test suite.</summary>
+	[TestClass]
+	public class DefaultServicesTests : BaseWebApiWebStackTests
+	{
+		[TestMethod]
+        public void GetService_PrefersServiceInDependencyInjectionContainer()
+        {
+            // Arrange
+            var config = new HttpConfiguration();
+            var defaultServices = new DefaultServices(config);
+            var filterProvider = new Mock<IActionValueBinder>().Object;
+            var mockDependencyResolver = new Mock<IDependencyResolver>();
+            mockDependencyResolver.Setup(dr => dr.GetService(typeof(IActionValueBinder))).Returns(filterProvider);
+            config.DependencyResolver = mockDependencyResolver.Object;
+
+            // Act
+            object service = defaultServices.GetService(typeof(IActionValueBinder));
+
+            // Assert
+            Object.ReferenceEquals( filterProvider, service );
+        }
+
+        [TestMethod]
+        public void GetService_CachesResultFromDependencyInjectionContainer()
+        {
+            // Arrange
+            var config = new HttpConfiguration();
+            var defaultServices = new DefaultServices(config);
+            var mockDependencyResolver = new Mock<IDependencyResolver>();
+            config.DependencyResolver = mockDependencyResolver.Object;
+
+            // Act
+            defaultServices.GetService(typeof(IActionValueBinder));
+            defaultServices.GetService(typeof(IActionValueBinder));
+
+            // Assert
+            mockDependencyResolver.Verify(dr => dr.GetService(typeof(IActionValueBinder)), Times.Once());
+        }
+
+		[TestMethod]
+        public void GetServices_PrependsServiceInDependencyInjectionContainer()
+        {
+            // Arrange
+            var config = new HttpConfiguration();
+            var defaultServices = new DefaultServices(config);
+
+            IEnumerable<object> servicesBefore = defaultServices.GetServices(typeof(IFilterProvider));
+            var filterProvider = new Mock<IFilterProvider>().Object;
+            var mockDependencyResolver = new Mock<IDependencyResolver>();
+            
+			mockDependencyResolver.Setup(dr => dr.GetServices(typeof(IFilterProvider))).Returns(new[] { filterProvider });
+            config.DependencyResolver = mockDependencyResolver.Object;
+
+            // Act
+            IEnumerable<object> servicesAfter = defaultServices.GetServices(typeof(IFilterProvider));
+
+            // Assert
+			Enumerable
+				.SequenceEqual(
+					new Object[] { filterProvider }.Concat( servicesBefore ),
+					servicesAfter
+				)
+				.ShouldBeTrue();
+        }
+
+		[TestMethod]
+        public void GetServices_CachesResultFromDependencyInjectionContainer()
+        {
+            // Arrange
+            var config = new HttpConfiguration();
+            var defaultServices = new DefaultServices(config);
+            var mockDependencyResolver = new Mock<IDependencyResolver>();
+            config.DependencyResolver = mockDependencyResolver.Object;
+
+            // Act
+            defaultServices.GetServices(typeof(IFilterProvider));
+            defaultServices.GetServices(typeof(IFilterProvider));
+
+            // Assert
+            mockDependencyResolver.Verify(dr => dr.GetServices(typeof(IFilterProvider)), Times.Once());
+        }
+	}
+}
