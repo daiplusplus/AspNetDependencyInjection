c3a3a58c5ae22fab9468019a25325744a5445999
Dai Rees <dai@dai.me>
2021-07-18 18:42:16 -0700

    Adding a stub ASP.NET Web-API project (without any MVC dependencies).

---
+++ SampleWebApiService/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+
+namespace SampleWebApiService
+{
+	public class RouteConfig
+	{
+		public static void RegisterRoutes( RouteCollection routes )
+		{
+			routes.Ignore( "{resource}.axd/{*pathInfo}" );
+
+			routes.MapHttpRoute(
+				name: "API Default",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
+}
---
+++ SampleWebApiService/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace SampleWebApiService
+{
+	public class WebApiApplication : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			GlobalConfiguration.Configure( WebApiConfig.Register );
+			RouteConfig.RegisterRoutes( RouteTable.Routes );
+		}
+	}
+}
