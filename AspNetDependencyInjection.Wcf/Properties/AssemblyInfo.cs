d418068261027bacefbc30fef9ce7a3d6a2532e5
Dai Rees <dai@dai.me>
2021-07-19 22:49:55 -0700

    Tidying up WCF project.

--- AspNetDependencyInjection.Wcf/Properties/AssemblyInfo.cs
+++ AspNetDependencyInjection.Wcf/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -45,3 +46,5 @@ using System.Runtime.InteropServices;
 	"e94586a5" )]
 
 */
+
+[assembly: CLSCompliant( isCompliant: true )]
